5cbc689a7d3d8c42301c187b16b58c60c5aa1c3e
Jon <jyorg@outlook.com>
2015-03-22 20:05:39 -0700

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
 
   has_secure_password
-    validates :password, length: { minimum: 6 }
-
+      validates :password, length: { minimum: 6 } 
   # Returns the hash digest of the given string.
  def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
